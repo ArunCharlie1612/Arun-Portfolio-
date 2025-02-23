83d14b016b8cba5349cf6342ceb018e795318312
ArunCharlie1612 <110811081+ArunCharlie1612@users.noreply.github.com>
2025-02-23 11:35:12 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,453 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  // typescript,
+  html,
+  css,
+  reactjs,
+  // redux,
+  tailwind,
+  // nodejs,
+  // mongodb,
+  git,
+  // figma,
+  // docker,
+  // meta,
+  // starbucks,
+  // tesla,
+  // shopify,
+  // carrent,
+  // jobit,
+  // tripguide,
+  // threejs,
+  jmc,
+  pauls,
+  ilife,
+  Accenture,
+  php,
+  oracle,
+  java,
+  bootstrap,
+  arun_pholio,
+  pascaline,
+  charlie_Dewi,
+  Best_Intern,
+  Indepth,
+  Intro_Front,
+  Java,
+  JS,
+  Moretan_Bay,
+  React,
+  Skyscanner,
+  Version_Control,
+  Qantler,
+  redux,
+  API,
+  jQuery,
+  Simploitel,
+  portfolio,
+  POS,
+  AdvancedReact
+} from "../assets";
+
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "certifications",
+    title: "Certifications",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "React Developer",
+    icon: web,
+  },
+  {
+    title: "Front-End Developer",
+    icon: mobile,
+  },
+  {
+    title: "JAVA Developer",
+    icon: backend,
+  },
+  {
+    title: "Web Designer | Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Core Java",
+    icon: java,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "PHP",
+    icon: php,
+  },
+  {
+    name: "Oracle",
+    icon: oracle,
+  },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "Redux",
+    icon: redux,
+  },
+  {
+    name: "API",
+    icon: API,
+  },
+  {
+    name: "jQuery",
+    icon: jQuery,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+
+const certi = [
+  {
+    name: "Introduction to Front-End Development",
+    company_name: "META",
+    source_code_link: Intro_Front,
+  },
+  {
+    name: "React Basics - 2023",
+    company_name: "META",
+    source_code_link: React,
+  },
+  {
+    name: "Programming with JavaScript - 2023",
+    company_name: "META",
+    source_code_link: JS,
+  },
+  {
+    name: "HTML & CSS Indepth - 2023",
+    company_name: "META",
+    source_code_link: Indepth,
+  },
+
+  {
+    name: "Version Control System - 2023",
+    company_name: "META",
+    source_code_link: Version_Control,
+  },
+  {
+    name: "Advanced React - 2023",
+    company_name: "META",
+    source_code_link: AdvancedReact,
+  },
+  {
+    name: "Java for Beginners - 2021",
+    company_name: "Udemy",
+    source_code_link: Java,
+  },
+  {
+    name: "Best Intern Award - 2022",
+    company_name: "ILife Technologies",
+    source_code_link: Best_Intern,
+  },
+  {
+    name: "Virtual Internship - Web Development",
+    company_name: "Moretan Bay",
+    source_code_link: Moretan_Bay,
+  },
+  {
+    name: "Virtual Internship - Front-End Engineering",
+    company_name: "SkyScanner",
+    source_code_link: Skyscanner,
+  },
+];
+
+const experiences = [
+  {
+    title: "Associate Software Engineer",
+    company_name: "Accenture, Pune",
+    icon: Accenture,
+    iconBg: "#f2f2f2",
+    date: "Feb 2024 - Present",
+    points: [
+      "As an Associate Software Engineer in a dynamic team, I specialize in building and delivering precise, data-driven reports for Fund Managers to enhance their trading strategies. Using BlackRock's Aladdin Explore, a leading tool in asset management, I work on developing complex reports by writing custom JavaScript scripts to meet business requirements and client needs. This involves requirements analysis, report development, and ensuring that the final output aligns with our clients' expectations regarding accuracy and clarity",
+      " Collaborating with cross-functional teams to analyze business requirements and transform them into effective reporting solutions",
+      " Writing JavaScript scripts to customize and automate reports in the Aladdin Explore environment for asset management and trading teams",
+      "Delivering high-quality, actionable insights for Fund Managers to aid in their trading decisions",
+      "Working in an organized, stress-free environment with a supportive team that encourages continuous learning and knowledge sharing",
+      "Opportunity to leverage my expertise in React.js and front-end technologies, while expanding my skillset with .NET and Microsoft DevOps tools",
+      "With a passion for innovative software solutions and a strong foundation in front-end development, I am committed to delivering efficient, reliable, and user-centric products that help clients achieve their financial objectives",
+    ],
+  }
+  ,
+  {
+    title: "Front-End Developer Intern",
+    company_name: "Qantler TEchnologies,Trichy.",
+    icon: Qantler,
+    iconBg: "#f2f2f2",
+    date: "May 2023 - Aug 2023",
+    points: [
+      "Implement and translate Figma designs into pixel - perfect, fully functional web applications using React.js, HTML, CSS, and Bootstrap.",
+      "Collaborate closely with the design team to understand project requirements, user experience, and interface design.",
+      "Optimize applications for maximum speed and scalability, ensuring exceptional performance across different devices and browsers.",
+      "Utilize JavaScript and jQuery to enhance interactivity and dynamic content on the frontend.",
+      "Integrate with backend APIs to fetch and display data, providing a seamless user experience through asynchronous communication.",
+      "Conduct thorough testing and debugging to identify and resolve any issues or bugs that may arise during development.",
+      "Implement responsive web design principles to ensure cross- device compatibility and an excellent user experience on both desktop and mobile devices.",
+      "Stay up - to - date with the latest web development trends, tools, and technologies, and share knowledge with the team to enhance overall skill sets.",
+      "Maintain code quality, organization, and documentation, ensuring clear and efficient code that is easy to understand and maintain by others.",
+      "Work collaboratively with the development team, product managers, and stakeholders to meet project deadlines and deliver high - quality products",
+    ],
+  },
+  {
+    title: "Front-End Developer Intern",
+    company_name: "ILife TEchnologies,Trichy.",
+    icon: ilife,
+    iconBg: "#f2f2f2",
+    date: "Sep 2022 - March 2023",
+    points: [
+      "Developing and maintaining web applications using HTML, CSS, Bootstrap and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "I was awarded as a 'Best Intern Award' by ILife Technologies."
+    ],
+  },
+  {
+    title: "Bachelor of Computer Science",
+    company_name: "Jamal Mohamed College, Trichy",
+    icon: jmc,
+    iconBg: "#f2f2f2",
+    date: "Aug 2020 - May 2023",
+    points: [
+      "Pursuing Bachelor of Computer Science with 9.4CGPA till 5th Semester.",
+      "Graduates of a Bachelor of Computer Science program have developed technical skills such as programming, software development, and database management. These skills are highly sought after in the job market.",
+      "Completed Some Projects like Commercial Websites and Good Looking Calculator using Web Technologies like HTML, CSS, Bootstrap and Basics of PHP.",
+      "Particpated in Many Technical Symposium and Got First Prize in Debugging Competition.",
+    ],
+  },
+  {
+    title: "HSC",
+    company_name: "St.Paul's Matriculation Higher Secondary School, Kovilpatti",
+    icon: pauls,
+    iconBg: "#f2f2f2",
+    date: "June 2019 - May 2020",
+    points: [
+      "I took Maths Biology Group and got 79.9% in my HSC.",
+      "I played captaincy in Throwball from 7th Std to 12th Std.",
+      "I have taken my team Upto Divisional Level three times under my Captaincy.",
+    ],
+  },
+  {
+    title: "SSC",
+    company_name: "St.Paul's Matriculation Higher Secondary School, Kovilpatti",
+    icon: pauls,
+    iconBg: "#f2f2f2",
+    date: "June 2017 - May-2018",
+    points: [
+      "I got 89.9% in my SSC.",
+      "Because of my effective Leadership, I was elected as an ASPL (Assistant School People Leader) in my school.",
+    ],
+  },
+];
+
+
+
+const projects = [
+  {
+    name: "POS",
+    description:
+      "The POS (Point of Sale) is the application with HTML,CSS,Bootstrap,jQuery and pure JS. I have done this in react also.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "jQuery",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: POS,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "React Portfolio",
+    description:
+      "A Portfolio created with the technologies like React.js, HTML, CSS, Tailwind, jQuery, JavaScript. I have done this in my college days.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "TailWind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "React.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "jQuery",
+        color: "green-text-gradient",
+      },
+    ],
+    image: portfolio,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Simploitel",
+    description:
+      "A Portfolio created with the technologies like React.js, HTML, CSS and without using any external Libraries. I have done this task for a company .",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+    ],
+    image: Simploitel,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Charlie Dewi",
+    description:
+      "A web design company is a valuable partner for businesses and individuals who want to establish an effective online presence and create a website that is user-friendly.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: charlie_Dewi,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Pascaline ",
+    description:
+      "A calculator web application like the Pascaline is a convenient and versatile tool for performing mathematical calculations and functions in a digital format.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "JS",
+        color: "White-text-gradient",
+      },
+    ],
+    image: pascaline,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Arun Photofolio",
+    description:
+      "A photography web application is a software program or online service that allows photographers to showcase and share their photos with others on the internet.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: arun_pholio,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, certi, experiences, projects };
